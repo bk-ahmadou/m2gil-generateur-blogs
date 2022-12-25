b316880fe019b45aa292973a1f05e5027c4f5ded
ahmadou <ahmadou.bachir@zenpark.com>
2022-12-25 18:43:28 +0100

    add dropdown

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,10 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
 
-// Write your JavaScript code.
+function showDrapdown() {
+  var x = document.getElementById("ttttt");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
